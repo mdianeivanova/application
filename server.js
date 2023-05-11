89fd9fad5918533b4176dc6b8ec8fde57f8ba347
mdianeivanova <80835955+mdianeivanova@users.noreply.github.com>
2023-05-11 17:23:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { getTransactions, addTransaction, deleteTransaction } = require('./TransactionManager');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(bodyParser.json());
 

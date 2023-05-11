6fe070dee3d4b0d56d4f4422ee41403ea393e53b
mdianeivanova <80835955+mdianeivanova@users.noreply.github.com>
2023-05-11 17:41:38 -0400

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { Transaction, getTransactions, addTransaction, deleteTransaction } = require('TransactionManager');
+const { Transaction, getTransactions, addTransaction, deleteTransaction } = require('../TransactionManager');
 const router = express.Router();
+
 router.get('/', (req, res) => {
   const transactions = getTransactions();
   res.json(transactions);
@@ -13,18 +14,22 @@ router.post('/', (req, res) => {
     res.status(400).send('Bad request: missing required fields');
     return;
   }
+
   const newTransaction = addTransaction({ date, description, amount, type });
   res.json(newTransaction);
 });
 
-router.delete(':transaction_id', (req, res) => {
+router.delete('/:transaction_id', (req, res) => {
   const transactionId = req.params.transaction_id;
   const deletedTransaction = deleteTransaction(transactionId);
+
   if (deletedTransaction) {
     res.status(204).send();
   } else {
     res.status(404).send('Transaction not found');
   }
 });
+
 module.exports = router;
+
 

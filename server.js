ead895806e094f1b488e36f18c76d81571fbb9c3
mdianeivanova <80835955+mdianeivanova@users.noreply.github.com>
2023-05-11 17:40:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getTransactions, addTransaction, deleteTransaction } = require('TransactionManager');
+const { getTransactions, addTransaction, deleteTransaction } = require('./TransactionManager');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-app.get('transactions', (req, res) => {
+app.get('/transactions', (req, res) => {
   const transactions = getTransactions();
   res.status(200).json(transactions);
 });
 
-app.post('transactions', (req, res) => {
+app.post('/transactions', (req, res) => {
   const transaction = req.body;
   const newTransaction = addTransaction(transaction);
   res.status(201).json(newTransaction);
 });
 
-app.delete('transactions/:transaction_id', (req, res) => {
+app.delete('/transactions/:transaction_id', (req, res) => {
   const transactionId = req.params.transaction_id;
   const deletedTransaction = deleteTransaction(transactionId);
   if (deletedTransaction) {
@@ -30,4 +30,3 @@ app.delete('transactions/:transaction_id', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
